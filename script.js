aaaf6acfde7b712f6cff0427713bd87c8c706465
Aniket Soni <theaniketsoni121@gmail.com>
2026-04-02 23:50:57 +0530

    Add file validation function

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+function validateFile() {
+    let file = document.getElementById("file").value;
+    if(file === ""){
+        alert("Upload file!");
+        return false;
+    }
+    return true;
+}
